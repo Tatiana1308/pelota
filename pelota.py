871f92464aa22f37ec75550ba729d93717f5ad66
Tatiana1308 <159279523+Tatiana1308@users.noreply.github.com>
2024-04-08 14:17:05 -0500

    Add files via upload

---
+++ pelota.py
@@ -0,0 +1,21 @@
+class pelota: 
+    def init(self):
+        self.x=0
+        self.y=0
+        self.radio=1
+        self.color="red"
+        self.tipo="circulo"
+        self.tipoRelleno="relleno"
+    def mover (self):
+        print("me estoy moviendo")  
+    
+    def rebotar(self):
+        pass 
+
+    def rebotar(self):
+        pass 
+
+    def dibujar(self):
+        pass
+    
+    
